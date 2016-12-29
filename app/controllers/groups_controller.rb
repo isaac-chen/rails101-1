0b73a5f9e8b6c6a670d72853754875b4fe42e348
isaac <237957459@qq.com>
2016-12-29 23:28:42 +0800

    add validation to title

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -17,9 +17,12 @@ end
 
   def create
     @group = Group.new(group_params)
-    @group.save
 
+    if @group.save
     redirect_to groups_path
+  else
+    render :new
+  end
 end
 
 def update
@@ -35,7 +38,7 @@ def destroy
   @group.destroy
   flash[:alert] = "Group deleted"
   redirect_to groups_path
-end 
+end
 
 private
 
